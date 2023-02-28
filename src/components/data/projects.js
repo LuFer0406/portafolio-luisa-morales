eaae0b03eb12345feebd4d8f3489e5b4a6582698
LuisaFernanda <luisamoralesocp@gmail.com>
2023-02-28 17:13:00 -0500

    portafolio-luisa-morales v2

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,5 +1,7 @@
 import proyectoImg1 from "../../assets/coinsApp.PNG"
-import proyectoImg2 from "../../assets/postsApp.PNG"
+import proyectoImg2 from "../../assets/covidApp.PNG"
+import proyectoImg3 from "../../assets/noteApp.PNG"
+
 export const projectData = [
     {
         id: 1,
@@ -12,12 +14,21 @@ export const projectData = [
     },
     {
         id: 2,
-        title: "Post Application",
-        about: "Aplicación FullStack",
+        title: "Covid Application",
+        about: "Consumo de API",
         tags: ["ReactJS", "NodeJS"],
-        demo: "https://warm-mandazi-fa2257.netlify.app/",
-        github: "https://github.com/LuFer0406/coinsApp",
+        demo: "https://main--incandescent-halva-1db273.netlify.app/",
+        github: "https://github.com/LuFer0406/covidApp",
         image: proyectoImg2
     },
+    {
+        id: 3,
+        title: "Notes Application",
+        about: "Aplicación de notas (CRUD) implementando socket.io",
+        tags: ["ReactJS", "NodeJS", "ExpressJS"],
+        demo: "https://main--superlative-beijinho-a41922.netlify.app/",
+        github: "https://github.com/LuFer0406/notesApp",
+        image: proyectoImg3
+    }
 
 ]
