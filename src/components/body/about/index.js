3183032cd55439494c34d2f6616d3d0e68cf2598
LuisaFernanda <luisamoralesocp@gmail.com>
2023-02-28 19:08:38 -0500

    portafolio-luisa-morales v4

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -8,7 +8,7 @@ function About() {
       <div className='about-top'>
         <div className='about-info'>
           <h1 className='about-center'>Hola, soy <span className='about-name'>Luisa Morales.</span></h1>
-          <h2 className='about-role'>Desarolladora Full Stack Junior.</h2>
+          <h2 className='about-role'>Desarrolladora Full Stack Junior.</h2>
           <p className='about-description'>Egresada de Técnico laboral en Programación de Sofware en Universidad Pontificia Bolivariana y estudiante de Tecnología en Análisis y Desarrollo de Software en Servicio Nacional de Aprendizaje (SENA).</p>
         </div>
       </div>
